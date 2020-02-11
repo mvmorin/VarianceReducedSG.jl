c6960113e2f911c968fb59e2bee4f7453b77bcf8
Martin Morin <martin.morin@control.lth.se>
2020-02-11 14:46:48 +0100

    Change mutable fields in logger to RefValue instead of length 1 vectors

--- src/loggers.jl
+++ src/loggers.jl
@@ -56,26 +56,26 @@ log!(l::ShowNewLine) = println(l.s)
 
 ################################################################################
 struct ShowTime{F} <: AbstractLogger
-	t0::Vector{F}
+	t0::Base.RefValue{F}
 	function ShowTime()
 		t0 = time()
-		new{typeof(t0)}([t0])
+		new{typeof(t0)}(Ref(t0))
 	end
 end
-initialize!(l::ShowTime) = l.t0[1] = time()
-finalize!(l::ShowTime) = @printf("Total time elapsed: %.5g\n", time() - l.t0[1])
-log!(l::ShowTime) = @printf("Time: %5.f, ", time() - l.t0[1])
+initialize!(l::ShowTime) = l.t0[] = time()
+finalize!(l::ShowTime) = @printf("Total time elapsed: %.5g\n", time() - l.t0[])
+log!(l::ShowTime) = @printf("Time: %5.f, ", time() - l.t0[])
 
 
 ################################################################################
 struct CacheFuncVal{F,T} <: AbstractLogger
 	f::F
-	val::Array{T,1}
-	CacheFuncVal(f::F,T=Float64) where {F} = new{F,T}(f,zeros(T,1))
+	val::Base.RefValue{T}
+	CacheFuncVal(f::F,T=Float64) where {F} = new{F,T}(f,Ref(zero(T)))
 end
 log!(l::CacheFuncVal,alg,iter,stage) =
-	l.val[1] = l.f(primiterates(alg),dualiterates(alg))
-(l::CacheFuncVal)(x,y) = l.val[1]
+	l.val[] = l.f(primiterates(alg),dualiterates(alg))
+(l::CacheFuncVal)(x,y) = l.val[]
 
 
 ################################################################################
@@ -91,19 +91,19 @@ log!(l::ShowFuncVal, alg, iter, stage) =
 struct StoreFuncVal{F,V} <: AbstractLogger
 	f::F
 	fvals::V
-	idx::Vector{Int}
-	StoreFuncVal(f::F, fv::V) where {F,V} = new{F,V}(f,fv,[1])
+	idx::Base.RefValue{Int}
+	StoreFuncVal(f::F, fv::V) where {F,V} = new{F,V}(f,fv,Ref(1))
 end
-initialize!(l::StoreFuncVal) = l.idx[1] = 1
+initialize!(l::StoreFuncVal) = l.idx[] = 1
 function log!(l::StoreFuncVal, alg, iter, stage)
-	i = l.idx[1]
+	i = l.idx[]
 	i > length(l.fvals) && return
 	l.fvals[i] = l.f(primiterates(alg), dualiterates(alg))
-	l.idx[1] += 1
+	l.idx[] += 1
 end
 function finalize!(l::StoreFuncVal)
 	# Zero out unused space
-	last = l.idx[1]
+	last = l.idx[]
 
 	fval = zero(eltype(l.fvals))
 	for i = last:length(l.fvals)
@@ -114,19 +114,19 @@ end
 ################################################################################
 struct StoreLogIterations{VI<:AbstractArray{Int}} <: AbstractLogger
 	iterations::VI
-	idx::Vector{Int}
-	StoreLogIterations(iterations::VI) where {VI} = new{VI}(iterations,[1])
+	idx::Base.RefValue{Int}
+	StoreLogIterations(iterations::VI) where {VI} = new{VI}(iterations,Ref(1))
 end
-initialize!(l::StoreLogIterations) = l.idx[1] = 1
+initialize!(l::StoreLogIterations) = l.idx[] = 1
 function log!(l::StoreLogIterations, alg, iter, stage)
-	i = l.idx[1]
+	i = l.idx[]
 	i > length(l.iterations) && return
 	l.iterations[i] = iter
-	l.idx[1] += 1
+	l.idx[] += 1
 end
 function finalize!(l::StoreLogIterations)
 	# Zero out unused space
-	last = l.idx[1]
+	last = l.idx[]
 
 	iter = zero(eltype(l.iterations))
 	for i = last:length(l.iterations)
